60c6364d3390a819db3a85045217ef4e968444f7
alpotapov <al.potapov@hotmail.com>
2015-06-13 18:03:48 +0200

    recent changes

--- software/hello.py
+++ software/hello.py
@@ -1,8 +1,21 @@
-import os
-from flask import Flask, Response
+# all the imports
+import sqlite3
+from flask import Flask, Response, request, session, g, redirect, url_for, \
+     abort, render_template, flash
 import urllib2
 
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 @app.route('/hook/payment_received')

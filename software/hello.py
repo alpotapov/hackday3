67621dab48f0fffb19fb02fed2924f1d53f51726
alpotapov <al.potapov@hotmail.com>
2015-06-13 19:34:14 +0200

    w

--- software/hello.py
+++ software/hello.py
@@ -1,31 +1,41 @@
 # all the imports
-import sqlite3
-from flask import Flask, request, Response
-import urllib2
+from flask import Flask, request, Response, redirect
+import datetime, time
 
 app = Flask(__name__)
 
 last_notification = ''
 
-@app.route('/hook/payment_received', methods=['POST', ])
+@app.route('/hook/payment_received', methods=['POST', 'GET'])
 def hook_payment_received():
-    if request.method == "POST":
-        timestamp = request.form['created_at']
-        id = request.form['id']
+    global last_notification
 
-        last_notification = timestamp
+    ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    last_notification = ts
+
+    # if request.method == "POST":
+    #     timestamp = request.form['created_at']
+    #     id = request.form['id']
+    #
+    #     last_notification = timestamp
 
     return ""
 
 
 @app.route('/status')
 def get_status():
-    return Response(response=last_notification, mimetype="text/plain")
+    if last_notification == '':
+        status = 'No data'
+    else:
+        status = last_notification
+    return Response(response=status, mimetype="text/plain")
 
 @app.route('/status/set/<timestamp>')
 def set_status(timestamp):
+    global last_notification
     last_notification = timestamp
+    return redirect('/status')
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '!Hello World!'

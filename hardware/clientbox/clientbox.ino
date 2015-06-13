089aa3fd568d50c44fab7989da05beffde237131
Ihor Andreicyhn <vidmak.case@gmail.com>
2015-06-13 14:25:53 +0200

    Coin detection with calibration

--- hardware/clientbox/clientbox.ino
+++ hardware/clientbox/clientbox.ino
@@ -1,21 +1,68 @@
+#include <SoftwareSerial.h>
+SoftwareSerial vSerial(10, 11); // RX, TX
 
-int s = 500;
-int ledPin = 13;
-int lightPin = A0; 
+const int ledPin = 13;
+const int lightPin = A0; 
+const int calibrationTime = 1000;
+const int calibrateMeasurmentCount = 10;
+const int s = 500;
 
+int avgLevel = 0;
+int minLevel = 1024;
+int maxLevel = 0;
+int sensitivity = 1024;
 void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+	Serial.begin(9600);
+	vSerial.begin(9600);  
+	pinMode(ledPin, OUTPUT);
+	calibrate();
 }
 
 void loop() {
-  Serial.println("Yo");
-  int light = analogRead(lightPin);
-  Serial.println(light);
-  digitalWrite(ledPin,LOW);
-  delay(s);
-  digitalWrite(ledPin,HIGH);
-  delay(s);
+	detectCoins();
 }
+
+void calibrate()
+{
+	vSerial.print("Calibration:");
+
+	int light = analogRead(lightPin);
+	int count = 1;
+	avgLevel = light;
+	for(int i=1;i<calibrateMeasurmentCount;i++)
+	{  
+		int light = analogRead(lightPin);
+		minLevel = min(minLevel,light);
+		maxLevel = max(maxLevel,light);
+		avgLevel = avgLevel + light;
+		count ++;
+		delay(calibrationTime/calibrateMeasurmentCount);
+	}
+	avgLevel = avgLevel / count;
+	vSerial.print("Avg:");
+	vSerial.println(avgLevel);
+	vSerial.print("Min:");
+	vSerial.println(minLevel);
+	vSerial.print("Min:");
+	vSerial.println(maxLevel);
+	sensitivity = (maxLevel - minLevel) / 2;
+}
+
+void detectCoins()
+{
+	int light = analogRead(lightPin);
+	if(light<930)
+	{
+		digitalWrite(ledPin,LOW);
+		vSerial.println("Coin");
+		delay(1000);
+		digitalWrite(ledPin,HIGH);
+	}	
+	vSerial.println(light);
+	delay(100);
+}
+
+
+
 
 

a7b45232d9ef9a739f49153277528ba50a7c6985
alpotapov <al.potapov@hotmail.com>
2015-06-14 00:08:50 +0200

    Minor fixes, added logo

--- software/hello.py
+++ software/hello.py
@@ -5,19 +5,23 @@ import datetime, time
 app = Flask(__name__)
 
 last_notification = ''
+last_transaction = None
+
+class Transaction:
+    def __init__(self, timestamp, id):
+        self.timestamp = timestamp
+        self.id = id
+
 
 @app.route('/hook/payment_received', methods=['POST', 'GET'])
 def hook_payment_received():
-    global last_notification
+    global last_notification, last_transaction
 
     ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     last_notification = ts
 
-    # if request.method == "POST":
-    #     timestamp = request.form['created_at']
-    #     id = request.form['id']
-    #
-    #     last_notification = timestamp
+    if request.method == "POST":
+        last_transaction = Transaction(request.form['created_at'], request.form['id'])
 
     return ""
 
@@ -30,18 +34,27 @@ def get_status():
         status = last_notification
     return Response(response=status, mimetype="text/plain")
 
+
 @app.route('/status/set/<timestamp>')
 def set_status(timestamp):
     global last_notification
     last_notification = timestamp
     return redirect('/status')
 
+
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+
+@app.route('/<link>')
+def internal_links(link):
+    return render_template(link)
+
 
 with app.test_request_context():
     url_for('static', filename='css/framework7.min.css')
     url_for('static', filename='css/my-app.css')
     url_for('static', filename='js/framework7.min.js')
     url_for('static', filename='js/my-app.js')
+    url_for('static', filename='img/dropcare-logo.png')

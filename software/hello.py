ed62f480e368a4a90b0248168bc4f9db8b892605
alpotapov <al.potapov@hotmail.com>
2015-06-13 16:14:52 +0200

    latest

--- software/hello.py
+++ software/hello.py
@@ -1,7 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, Response
+import urllib2
 
 app = Flask(__name__)
+
+
+@app.route('/hook/payment_received')
+def hook_payment_received():
+    with open("status.txt", "a") as myfile:
+        myfile.write("Payment Recieved")
+        
+    return ""
+
 
 @app.route('/')
 def hello():

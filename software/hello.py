159f2dcdd38e54f27ab0ba2b258f608c1cc9bd00
alpotapov <al.potapov@hotmail.com>
2015-06-14 11:59:57 +0200

    Transactions

--- software/hello.py
+++ software/hello.py
@@ -1,6 +1,8 @@
 # all the imports
 from flask import Flask, request, Response, redirect, url_for, render_template
 import datetime, time
+import urllib2, base64
+import paymill
 
 app = Flask(__name__)
 
@@ -40,6 +42,26 @@ def set_status(timestamp):
     global last_notification
     last_notification = timestamp
     return redirect('/status')
+
+
+@app.route('/transaction')
+@app.route('/transaction/<token>')
+def transaction(amount=5, token="098f6bcd4621d373cade4e832627b4f6"):
+    paymill_context = paymill.PaymillContext('11870062161c9d3d64f859af9746f7b5')
+
+    # payment_service = paymill_context.get_payment_service()
+    # payment_with_token = payment_service.create()
+    # token='12a46bcd462sd3r3care4e8336ssb4f5'
+
+    transaction_service = paymill_context.get_transaction_service()
+    transaction_with_token = transaction_service.create_with_token(
+        token=token,
+        amount=amount*100,
+        currency='EUR',
+        description='Payment'
+    )
+
+    return ""
 
 
 @app.route('/')

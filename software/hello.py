0bec155ac6fcd31b69a64628dd064f84834b0db0
alpotapov <al.potapov@hotmail.com>
2015-06-14 13:03:13 +0200

    Added images, minor bugfixing

--- software/hello.py
+++ software/hello.py
@@ -3,11 +3,17 @@ from flask import Flask, request, Response, redirect, url_for, render_template
 import datetime, time
 import urllib2, base64
 import paymill
+import random
 
 app = Flask(__name__)
 
 last_notification = ''
 last_transaction = None
+
+paymill_tokens = ['098f6bcd4621d373cade4e832627b4f6', '12a46bcd462sd3r3care4e8336ssb4f5']
+paymill_context = paymill.PaymillContext('11870062161c9d3d64f859af9746f7b5')
+paymill_last_token = None
+paymill_last_transaction = None
 
 class Transaction:
     def __init__(self, timestamp, id):
@@ -45,23 +51,37 @@ def set_status(timestamp):
 
 
 @app.route('/transaction')
-@app.route('/transaction/<token>')
-def transaction(amount=5, token="098f6bcd4621d373cade4e832627b4f6"):
-    paymill_context = paymill.PaymillContext('11870062161c9d3d64f859af9746f7b5')
+def transaction(amount=5):
+    global paymill_last_transaction, paymill_last_token
 
-    # payment_service = paymill_context.get_payment_service()
-    # payment_with_token = payment_service.create()
-    # token='12a46bcd462sd3r3care4e8336ssb4f5'
+    if paymill_last_transaction is not None:
+        if time.time() - paymill_last_transaction < 30:
+            new_token = paymill_last_token
+            while new_token == paymill_last_token:
+                new_token = random.choice(paymill_tokens)
+
+            token = new_token
+        else:
+            token = paymill_last_token
+    else:
+        token = random.choice(paymill_tokens)
 
     transaction_service = paymill_context.get_transaction_service()
-    transaction_with_token = transaction_service.create_with_token(
-        token=token,
-        amount=amount*100,
-        currency='EUR',
-        description='Payment'
-    )
+    try:
+        transaction_with_token = transaction_service.create_with_token(
+            token=token,
+            amount=amount*(100 + random.randint(1,2)),
+            currency='EUR',
+            description='Payment'
+        )
+        paymill_last_transaction = time.time()
+        paymill_last_token = token
+        status = "OK"
+    except:
+        status = "FAIL"
+        pass
 
-    return ""
+    return Response(response=status, mimetype="text/plain")
 
 
 @app.route('/')
